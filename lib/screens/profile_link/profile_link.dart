eb926827a2c2da72822e360fd521905ccc86e7d3
Mobileappdev <mobileappdev47@gmail.com>
2022-05-20 17:04:07 +0530

    profile

--- lib/screens/profile_link/profile_link.dart
+++ lib/screens/profile_link/profile_link.dart
@@ -56,7 +56,7 @@ class _ProfileLinkState extends State<ProfileLink> {
                       border: Border.all(color: Theme.of(context).primaryColor)
                     ),
                     child: Text(
-                      map.isEmpty ? "" : map['data']['url'],
+                      map.isEmpty ? "" : map['data']['url'].toString(),
                       style: Theme.of(context)
                           .textTheme
                           .subtitle2!
